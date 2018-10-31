e5783c8115a1456ce7ec748dfbdffc6796691805
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 13:12:30 +0100

    Update swap/src/Exercises/test_swap.c

--- swap/src/Exercises/test_swap.c
+++ swap/src/Exercises/test_swap.c
@@ -3,17 +3,29 @@
 #include "swap.h"
 
 int main() {
-	int a,b;
+	int tmp_a,tmp_b, i;
+    int test_val_a[3]={5,8,45};
+    int test_val_b[3]={5,6,13};
+    int ok=1;
 	
-	a=5;
-	b=6;
-	echange(&a,&b);
-	if (a==6 && b==5) {
-		printf("TECHIO> success true\n");
-	}
-	else {
+    for (i=0;i<3;i++) {
+		
+		tmp_a=test_val_a[i];
+		tmp_b=test_val_b[i];
+		echange(&tmp_a,&tmp_b);
+		if (tmp_b=test_val_a[i] && tmp_a=test_val_b[i]) {
+            ok = ok && 1;
+        }
+        else {
+            ok = ok && 0;
+            printf("TECHIO> message --channel \"TEST ERREUR\" 'Error function fail test (%d)'\n",i);
+        }
+    }
+        
+    if(ok)
+        printf("TECHIO> success true\n");
+    else  
 		printf("TECHIO> success false\n");
-	}
 	
 	return 0;
 }
