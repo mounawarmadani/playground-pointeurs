aea8a8a50c8b2aec3e998a1bfc75965c28dfac5a
Mounawar <mounawar.madani@hepl.be>
2018-11-03 22:31:32 +0100

    test prime

--- markdowns/ex3.md
+++ markdowns/ex3.md
@@ -1,3 +1,6 @@
 # Exercice 3
-
+ 
 Générer la liste des x premier nombre premier
+## Résolution
+
+@[Fonction d'échange]({"stubs": ["src/Exercise3/prime.c"],"command": "sh /project/target/run.sh"})
---
+++ prime/src/Exercise3/test_prime.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "prime.c"
+
+int _is_prime(int nb)
+{
+  int i, test;
+  test = 1;
+
+ for (i = 2; i < nb; i++)
+    if (nb % i == 0) test = 0;
+ return test;
+}
+
+int test_prime()
+{
+    int ok=1;
+    if(_is_prime(3) != is_prime(3))
+        ok=0;
+    return ok;
+    
+}
+
+int main(void) {
+
+    //int prime_test[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    ok=test_prime();
+    if(!ok)
+    {
+        printf("TECHIO> message --channel \"TEST ERREUR\" 'Error function fail test '\n");
+    }
+    if(ok)
+        printf("TECHIO> success true\n");
+    else  
+		printf("TECHIO> success false\n");
+  return 0;
+}
+
+
+
